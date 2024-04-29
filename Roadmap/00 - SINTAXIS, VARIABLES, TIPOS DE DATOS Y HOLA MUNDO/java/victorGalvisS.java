7bdffbd340abbe3247556ae42013c7770d5cce81
victorGalviss <vicman1987@gmail.com>
2024-04-29 13:27:13 +0200

    Mi primer pull-request en retos-programacion #00/java/victorGalviss.java

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/victorGalvisS.java
@@ -0,0 +1,34 @@
+/**
+ * VictorGalviss #00 victorGalvisS.java
+ */
+
+// Documentation en https://www.java.com/
+//Types of comments
+
+// One Line
+
+/**
+ * Multiplex line or block
+ */
+
+public class victorGalvisS {
+
+    //Constants
+    public static final String CONSTANTS = "I am Sotfware Developer";
+
+    public static void main(String[] args) {
+
+        //Variables
+        String variableOne = "Java";
+        boolean variableTwo = false;
+        int variableThree = 1252878;
+        float variableFour = 3.14172568F;
+        double variableFive = 10552.2052158851852585625;
+        long variableSix = variableThree * 1225;
+        char variableSevent = 'C';
+        byte variableHeight = 125;
+
+        System.out.println("Hello, " + variableOne);
+
+    }
+}
